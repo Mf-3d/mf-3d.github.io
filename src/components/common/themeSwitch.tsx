a41c871191a98af5144aa5f85748d241f6164386
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-01-04 18:20:36 +0900

    🐛 テーマスイッチが正常に動作しない問題を修正

--- src/components/common/themeSwitch.tsx
+++ src/components/common/themeSwitch.tsx
@@ -5,7 +5,7 @@ import { useTheme } from "next-themes";
 
 export const ThemeSwitch = () => {
   const [mounted, setMounted] = useState(false);
-  const { resolvedTheme, setTheme } = useTheme();
+  const { theme, setTheme } = useTheme();
 
   useEffect(() => {
     setMounted(true);
@@ -17,7 +17,7 @@ export const ThemeSwitch = () => {
 
   return (
     <select
-      value={resolvedTheme}
+      value={theme}
       onChange={(e) => setTheme(e.target.value)}
       className="border-2 border-black dark:border-white"
     >
