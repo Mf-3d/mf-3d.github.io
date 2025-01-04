5572caf837b6a81f9fab3340b5a64a8dbc49897e
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-01-04 16:58:45 +0900

    ❓ (?)

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        and: [/\.(js|ts)x?$/],
+      },
+      use: [
+        {
+          loader: '@svgr/webpack',
+          options: {
+            svgo: false, // 圧縮を無効にする設定
+          },
+        },
+      ],
+    });
+
+    return config;
+  },
+  images: {
+    disableStaticImages: true, // importした画像の型定義設定を無効にする設定
+  },
 };
 
 export default nextConfig;
