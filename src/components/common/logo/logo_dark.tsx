aadf05e1b2056f4f1064267281a22c083c2c8aa4
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-01-04 16:56:47 +0900

    ✨ ロゴのコンポーネントを追加

---
+++ src/components/common/logo/logo_dark.tsx
@@ -0,0 +1,8 @@
+import Icon from "public/images/logo_dark.svg";
+const LogoDark = (): JSX.Element => {
+  return (
+    <LogoDark />
+  );
+};
+
+export default LogoDark;
---
+++ src/components/common/logo/logo_light.tsx
@@ -0,0 +1,8 @@
+import Icon from "public/images/logo_light.svg";
+const LogoLight = () => {
+  return (
+    <LogoLight />
+  );
+};
+
+export default LogoLight;
