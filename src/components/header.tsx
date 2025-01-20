8dc7d3a78b7e6bf92d533bd2b27eacdf2a98f414
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-01-20 19:55:34 +0900

    🎨 不安になってきた😟😟

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -10,14 +10,14 @@ import { Tooltip } from './common/Tooltip';
 const Header = (): JSX.Element => {
   return (
     <header className="border-b-2 border-neutral-100 dark:border-neutral-900">
-      <h1 className="title"><Logo /></h1>
+      <h1 className="header__logo"><Logo /></h1>
       <nav className="nav">
-      <ul className="menu-group">
-        <li className="menu-item  !relative top-0.5"><Link href="/"><Tooltip tooltipText="ホーム"><LuHouse /></Tooltip></Link></li>
-        <li className="menu-item"><Link href="#">About</Link></li>
-        <li className="menu-item !relative top-0.5"><ThemeSwitch /></li>
-      </ul>
-    </nav>
+        <ul className="header__menu__group">
+          <li className="header__menu__item  !relative top-0.5"><Link href="/"><Tooltip tooltipText="ホーム"><LuHouse /></Tooltip></Link></li>
+          <li className="header__menu__item"><Link href="#">About</Link></li>
+          <li className="header__menu__item !relative top-0.5"><ThemeSwitch /></li>
+        </ul>
+      </nav>
     </header>
   );
 };
