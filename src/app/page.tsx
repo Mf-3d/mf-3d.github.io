031fdfab9e1b031c2f75ef5ecb46ef2e02dde759
mf7cli <84224913+Mf-3d@users.noreply.github.com>
2025-01-20 23:50:42 +0900

    🚧 古のmarquee要素をいまさら実装？

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,18 @@
-import Maintenance from "@/components/common/maintenance";
+"use client";
+
+import { useState, useEffect } from 'react';
+import { useTheme } from "next-themes";
+import Marquee from "react-fast-marquee";
 
 export default function Page() {
+  const { resolvedTheme } = useTheme();
+
   return (
     <main>
       <div className="flex justify-center">
-        <Maintenance />
+        <Marquee pauseOnHover={true} speed={40} gradient={true} gradientColor={resolvedTheme === "light" ? "white" : "rgb(10,10,10)"}>
+          <p>ここの文字がアニメーションします</p>
+        </Marquee>
       </div>
     </main>
   );
